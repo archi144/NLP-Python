eda2b7c8b2fb9ca099f1c556e7501a0d9b6ab113
Mechetin Artur <Artur.Mechetin@leroymerlin.ru>
2021-08-02 18:02:17 +0300

    feat: add spaCy

---
+++ spaCy/spaCy.py
@@ -0,0 +1,20 @@
+import spacy
+from spacy.symbols import ORTH, NORM
+
+nlp = spacy.load('en_core_web_md')
+special_case = [{ORTH: 'Frisco', NORM: 'San Francisco'}]
+nlp.tokenizer.add_special_case(u'Frisco', special_case)
+doc = nlp('I have flown to LA. Now I am fly to Frisco.')
+for sent in doc.sents:
+    if 'fly' in sent.lemma_:
+        result = {}
+        for token in sent:
+            if token.dep_ == 'ROOT' and token.tag_ != 'VBN':
+                result['travel_method'] = token.lemma_
+            elif token.dep_ == 'pobj':
+                result['target_city'] = token.norm_
+        else:
+            if '' in result.values():
+                pass
+            else:
+                print(list(result.values()))
